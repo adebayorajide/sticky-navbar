e073e294684e940ab75b8a392c5dd6dbb4c9be17
Ajide Tosin Adebayo <47295073+adebayorajide@users.noreply.github.com>
2023-01-25 23:50:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const navbarEl = document.querySelector('.navbar');
+
+const bottomContainerEl = document.querySelector('.bottom-container')
+console.log(bottomContainerEl.offsetTop);
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight - 50){
+        navbarEl.classList.add('active')
+    } else {
+        navbarEl.classList.remove('active')
+    }
+})
